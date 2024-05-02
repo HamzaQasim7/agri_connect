90c78df53ce4227a0a0f112419d8f3e2ac6ca8f9
WhalySoft <hamzaqasim.co@gmail.com>
2024-05-02 12:07:25 +0500

    fix the sign up bug

---
+++ lib/utils/user_message_helper.dart
@@ -0,0 +1,14 @@
+import 'package:farmassist/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class UserMessageHelper {
+  static void showSnackBar(BuildContext context, String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: AppTheme.nearlyGreen,
+        behavior: SnackBarBehavior.floating,
+        content: Text(msg),
+      ),
+    );
+  }
+}
